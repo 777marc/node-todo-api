047239501b236b86e577cea4aad6d7e9afba0cf1
Marc Mendoza <777marc@gmail.com>
2018-11-02 06:33:34 -0400

    fix

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -18,9 +18,21 @@ MonogoClient.connect('mongodb://localhost:27017/TodoApp',(err, db) => {
   // });
 
   //findOneAndDelete
-  db.collection('Todos').findOneAndDelete({text: 'Something to do'}).then((result) => {
-    console.log(result);
-  });
+  // db.collection('Todos').findOneAndDelete({text: 'Something to do'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  //delete many
+  // db.collection('Users').deleteMany({name: 'Marc Mendoza'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  //findOneAndDelete
+  db.collection('Users').findOneAndDelete({
+    _id: new ObjectID('5bdb98837dd0649b415f5238')
+  }).then((result) => {
+    console.log(JSON.stringify(result, undefined, 2));
+  });  
 
   db.close();
 });
